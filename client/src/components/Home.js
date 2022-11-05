11d9442a8a48c1bf5a0ea466e374868c4fa9f0d1
lsanc0043 <lsanc0043@gmail.com>
2022-11-04 19:24:07 -0700

    draw desk

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -32,7 +32,38 @@ const Home = () => {
       drawLine(485 + xChange, 600 + yChange, 232, 770, color, width, 2); // diagonal wall 1
     };
 
+    const drawDesk = (color, width, rough, xChange, yChange) => {
+      drawLine(500 + xChange, 330 + yChange, 200 + xChange, 480 + yChange, color, width, rough); // left diagonal
+      drawLine(800 + xChange, 330 + yChange, 500 + xChange, 480 + yChange, color, width, rough); // right diagonal
+      drawLine(800 + xChange, 350 + yChange, 500 + xChange, 500 + yChange, color, width, rough); // right diagonal - edge
+      drawLine(800 + xChange, 330 + yChange, 500 + xChange, 330 + yChange, color, width, rough); // top horizontal
+      drawLine(500 + xChange, 480 + yChange, 200 + xChange, 480 + yChange, color, width, rough); // bottom horizontal
+      drawLine(500 + xChange, 500 + yChange, 200 + xChange, 500 + yChange, color, width, rough); // bottom horizontal - edge
+      drawLine(200 + xChange, 480 + yChange, 200 + xChange, 500 + yChange, color, width, rough); // bottom left vertical - edge
+      drawLine(500 + xChange, 480 + yChange, 500 + xChange, 500 + yChange, color, width, rough); // bottom right vertical - edge
+      drawLine(800 + xChange, 330 + yChange, 800 + xChange, 350 + yChange, color, width, rough); // top right vertical - edge
+  
+      drawLine(800 + xChange, 350 + yChange, 800 + xChange, 605 + yChange, color, 0.3, 1); // top right leg - outward edge 575-320 = 255
+      drawLine(790 + xChange, 360 + yChange, 790 + xChange, 615 + yChange, color, 0.3, 1); // top right leg - middle edge
+      drawLine(775 + xChange, 360 + yChange, 775 + xChange, 615 + yChange, color, 0.3, 1); // top right leg - inward edge 615-360 = 255
+      drawLine(775 + xChange, 615 + yChange, 790 + xChange, 615 + yChange, color, 0.3, 1); // top right leg - bottom horizontal edge
+      drawLine(800 + xChange, 605 + yChange, 790 + xChange, 615 + yChange, color, 0.3, 1); // top right leg - bottom horizontal edge
+  
+      drawLine(510 + xChange, 490 + yChange, 510 + xChange, 770 + yChange, color, 0.3, 1); // bottom right leg - outward edge
+      drawLine(500 + xChange, 500 + yChange, 500 + xChange, 780 + yChange, color, 0.3, 1); // bottom right leg - middle edge
+      drawLine(485 + xChange, 500 + yChange, 485 + xChange, 780 + yChange, color, 0.3, 1); // bottom right leg - inward edge
+      drawLine(485 + xChange, 780 + yChange, 500 + xChange, 780 + yChange, color, 0.3, 1); // top right leg - bottom horizontal edge
+      drawLine(500 + xChange, 780 + yChange, 510 + xChange, 770 + yChange, color, 0.3, 1); // top right leg - bottom horizontal edge
+  
+      drawLine(200 + xChange, 500 + yChange, 200 + xChange, 780 + yChange, color, 0.3, 1); // bottom left leg - outward edge
+      drawLine(215 + xChange, 500 + yChange, 215 + xChange, 780 + yChange, color, 0.3, 1); // bottom left leg - middle edge
+      drawLine(225 + xChange, 500 + yChange, 225 + xChange, 770 + yChange, color, 0.3, 1); // bottom left leg - inward edge
+      drawLine(200 + xChange, 780 + yChange, 215 + xChange, 780 + yChange, color, 0.3, 1); // top right leg - bottom horizontal edge
+      drawLine(215 + xChange, 780 + yChange, 225 + xChange, 770 + yChange, color, 0.3, 1); // top right leg - bottom horizontal edge
+    };
+
     drawRoom("#ebebeb", 0.3, 1, 0, 30);
+    drawDesk("#ebebeb", 0.5, 2.5, 0, 30);
   });
 
   return (
