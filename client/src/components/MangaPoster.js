79724aca302c977a933f345144c82817b7ce718a
lsanc0043 <lsanc0043@gmail.com>
2022-11-04 17:46:40 -0700

    manga modal for corresponding clicked manga pops out

--- client/src/components/MangaPoster.js
+++ client/src/components/MangaPoster.js
@@ -1,7 +1,10 @@
 import { useState, useEffect } from "react";
+import MangaModal from "./MangaModal";
 
 const MangaPoster = () => {
   const [allMangas, setAllMangas] = useState([]);
+  const [selectedManga, setSelectedManga] = useState({});
+  const [showModal, setShowModal] = useState(false);
 
   // retrieve all the mangas
   const getMangas = async () => {
@@ -16,13 +19,19 @@ const MangaPoster = () => {
 
   return (
     <div className="container">
+      <MangaModal
+        showModal={showModal}
+        setShowModal={setShowModal}
+        selectedManga={selectedManga}
+      />
       {/* map all mangas */}
       {allMangas.map((manga, index) => {
         return (
           <button
             key={index}
             onClick={() => {
-              console.log(manga.id);
+              setSelectedManga(manga);
+              setShowModal(true);
             }}
             className="manga-selection"
           >
