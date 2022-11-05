ccd484f8aea677cff3d00943bbe7b6a3e3893378
lsanc0043 <lsanc0043@gmail.com>
2022-11-04 18:29:46 -0700

    added password validation for registration

--- client/src/components/LoginOrRegister.js
+++ client/src/components/LoginOrRegister.js
@@ -11,6 +11,7 @@ const LoginOrRegister = ({
   const [allUsers, setAllUsers] = useState([]); // list of all users
   const emailAvailable = [0]; // is the email available? [0] = false, [1] = true
   const userAvailable = [0]; // is the username available? [0] = false, [1] = true
+  const passwordValidity = [0, 0, 0, 0]; // is the password valid, [1, 1, 1, 1] = true, four requirements
 
   const [loginInfo, setLoginInfo] = useState({
     // saves the user typed info for both login and registration
@@ -44,6 +45,23 @@ const LoginOrRegister = ({
         0
       ) {
         userAvailable[0] = 1; // checks if the username the user wants to use already exists
+      }
+      // PASSWORD VALIDATION
+      if (loginInfo.password.length >= 8) {
+        // 1. greater than 8 characters
+        passwordValidity[0] = 1;
+      }
+      if (loginInfo.password.search(/[A-Z]/) !== -1) {
+        // 2. contains at least 1 capital letter
+        passwordValidity[1] = 1;
+      }
+      if (loginInfo.password.search(/[0-9]/) !== -1) {
+        // 3. contains at least 1 number
+        passwordValidity[2] = 1;
+      }
+      if (loginInfo.password === loginInfo.confirmPassword) {
+        // 4. both password fields match
+        passwordValidity[3] = 1;
       }
     }
     // sets input field values to the corresponding loginInfo object property
@@ -145,6 +163,71 @@ const LoginOrRegister = ({
               </>
             )}
           </form>
+          <div
+            style={{ display: login && !register ? "none" : "block" }}
+            className="pw-requirements"
+          >
+            <p>Password Requirements: </p>
+            minimum 8 characters
+            <svg viewBox="0 0 26 26" width="20px">
+              {passwordValidity[0] === 0 ? (
+                <>
+                  <path className="ex" d="M 18 7 L 7 18"></path>
+                  <path className="ex" d="M 7 7 L 18 18"></path>
+                </>
+              ) : (
+                <path
+                  className="checks"
+                  d="M6.5 13.5L10 17 l8.808621-8.308621"
+                />
+              )}
+            </svg>
+            <br />
+            at least one uppercase letter
+            <svg viewBox="0 0 26 26" width="20px">
+              {passwordValidity[1] === 0 ? (
+                <>
+                  <path className="ex" d="M 18 7 L 7 18"></path>
+                  <path className="ex" d="M 7 7 L 18 18"></path>
+                </>
+              ) : (
+                <path
+                  className="checks"
+                  d="M6.5 13.5L10 17 l8.808621-8.308621"
+                />
+              )}
+            </svg>
+            <br />
+            at least one number
+            <svg viewBox="0 0 26 26" width="20px">
+              {passwordValidity[2] === 0 ? (
+                <>
+                  <path className="ex" d="M 18 7 L 7 18"></path>
+                  <path className="ex" d="M 7 7 L 18 18"></path>
+                </>
+              ) : (
+                <path
+                  className="checks"
+                  d="M6.5 13.5L10 17 l8.808621-8.308621"
+                />
+              )}
+            </svg>
+            <br />
+            both passwords match
+            <svg viewBox="0 0 26 26" width="20px">
+              {passwordValidity[3] === 0 ? (
+                <>
+                  <path className="ex" d="M 18 7 L 7 18"></path>
+                  <path className="ex" d="M 7 7 L 18 18"></path>
+                </>
+              ) : (
+                <path
+                  className="checks"
+                  d="M6.5 13.5L10 17 l8.808621-8.308621"
+                />
+              )}
+            </svg>
+          </div>
         </Modal.Body>
         <Modal.Footer className="user-modal">
           {/* redirects to register modal, resets loginInfo & error message*/}
