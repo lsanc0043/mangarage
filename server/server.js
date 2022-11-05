9a6563ce4a918d3001fbf26552ee180d66752da2
Linda Sanchez <106147236+lsanc0043@users.noreply.github.com>
2022-11-04 20:12:59 -0700

    Revert "revert login register"

--- server/server.js
+++ server/server.js
@@ -1,15 +1,20 @@
-import express from 'express'; 
-import cors from 'cors'; 
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
 import mangasRouter from "./routes/manga.js";
+import usersRouter from "./routes/users.js";
 
-const app = express(); 
-const PORT = 4020; 
+const app = express();
+const PORT = 4020;
 
-app.use(cors()); 
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use("/manga", mangasRouter);
+app.use("/users", usersRouter);
 
-app.get('/', (req, res) => {
-	res.send('hello from server.js in the backend'); 
-}); 
+app.get("/", (req, res) => {
+  res.send("hello from server.js in the backend");
+});
 
 app.listen(PORT, () => console.log(`sup, you are listening to port ${PORT}`));
