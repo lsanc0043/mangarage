24ebed2e3e2d27dc6be68a293df048bd6f0b062d
lsanc0043 <lsanc0043@gmail.com>
2022-11-04 17:23:34 -0700

    get request from api, search functionality works

---
+++ server/routes/manga.js
@@ -0,0 +1,34 @@
+import fetch from "node-fetch";
+import { Router } from "express";
+const router = Router();
+
+// helper function to fetch data from API
+const fetchData = async (url) => {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  } catch (e) {
+    console.log("helper function get", e);
+  }
+};
+
+const getManga = async (search) => {
+  console.log("search input", search);
+  // grabs list of top 10 matching searches
+  const allData = await fetchData(
+    `https://api.mangadex.org/manga?limit=10&title=${search}`
+  );
+  const mangaData = allData.data.filter((manga) => manga.type === "manga"); // removes doujinshis or one-shots
+  // grabs the ONE manga that matches the searched name the most
+  return mangaData;
+};
+
+// get request
+router.get("/post/:search", async (req, res) => {
+  const search = req.params.search;
+  const results = await getManga(search);
+  res.send(results);
+});
+
+export default router;
--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 import express from 'express'; 
 import cors from 'cors'; 
+import mangasRouter from "./routes/manga.js";
 
 const app = express(); 
 const PORT = 4020; 
 
 app.use(cors()); 
+app.use("/manga", mangasRouter);
 
 app.get('/', (req, res) => {
 	res.send('hello from server.js in the backend'); 
