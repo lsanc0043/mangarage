87ca07234ddb7c5d398d94c6bcca08da64d3d2b5
lsanc0043 <lsanc0043@gmail.com>
2022-11-18 14:14:53 -0800

    work out cors

--- server/server.js
+++ server/server.js
@@ -18,6 +18,10 @@ app.use(express.static(REACT_BUILD_DIR));
 const PORT = process.env.PORT || 4020;
 
 app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use("/manga", mangasRouter);
@@ -32,29 +36,6 @@ app.get("/clear", async (req, res) => {
   await db.any("DROP TABLE IF EXISTS readmangas", [true]);
   await db.any("DROP TABLE IF EXISTS manga", [true]);
   await db.any("DROP TABLE IF EXISTS users", [true]);
-  res.send({ type: "success" });
-});
-
-app.get("/initialize", async (req, res) => {
-  console.log("request received");
-  await db.any("DROP TABLE IF EXISTS readmangas", [true]);
-  await db.any("DROP TABLE IF EXISTS manga", [true]);
-  await db.any("DROP TABLE IF EXISTS users", [true]);
-  await db.any(
-    "CREATE TABLE IF NOT EXISTS manga (id serial primary key, title text not null unique, author text not null, year text, status text,  last_updated text, description text, genres text[], cover text not null, characters text[])",
-    [true]
-  );
-  await db.any(
-    "CREATE TABLE IF NOT EXISTS users (id serial primary key, username text unique, email text unique not null, password text not null, last_login TIMESTAMP DEFAULT CURRENT_TIMESTAMP)",
-    [true]
-  );
-  await db.any(
-    "CREATE TABLE IF NOT EXISTS readmangas (user_id int not null, manga_id int not null, rating decimal not null, PRIMARY KEY ( user_id, manga_id), FOREIGN KEY (user_id) REFERENCES users(id) ON UPDATE CASCADE, FOREIGN KEY (manga_id) REFERENCES manga(id) ON UPDATE CASCADE)",
-    [true]
-  );
-  for (let i = 0; i < 48; i++) {
-    await db.query(`${MangaDump[i]}`, [true]);
-  }
   res.send({ type: "success" });
 });
 
